3a69e2992d73a6482d950c3e66eb7002e425fc6b
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-03 09:38:45 +0530

    Commiting modified files

--- FacebookPosts/settings.py
+++ FacebookPosts/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'IMDB.apps.ImdbConfig',
     'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
